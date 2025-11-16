c85b5bdd475106af13bc7e0c33160f28e27ed2bd
IsmailHaddouch <smailhaddouch21@gmail.com>
2025-11-16 18:29:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+
+  
+  return (
+    <>
+    <BrowserRouter>
+    
+    
+    
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
